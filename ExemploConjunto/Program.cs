8ed0c5d9dd68b6f930ce8136ae8d604edc1def78
hitaichou <holyfernandes@yahoo.co.uk>
2020-02-08 09:49:04 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploConjunto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploConjunto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
